214a1a78d929ff84956cdee1554fd1f40d596af1
Osman Haider <osmanhaider159@gmail.com>
2021-05-19 00:27:41 +0600

    api key from style complete

--- assets/admin/src/user-from/components/ApiKeyFrom.js
+++ assets/admin/src/user-from/components/ApiKeyFrom.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default class ApiKeyFrom extends React.Component {
   render() {
     return (
-      <div className="api-key-form">
-        <form>
-          <label for="key">API KEY:</label>
-          <input class="regular-text" type="text" name="key"></input>
-          <input type="submit" value="Save"></input>
-        </form>
+      <div>
+          <h1 className="settings-heading">Settings</h1>
+          <React.Fragment>
+          <CssBaseline />
+            <Container className="test">
+              <Typography className="from-container" component="div" style={{ backgroundColor: '#cfe8fc', height: '80vh', width:"80vh"}}>
+              <strong>Put your API key here</strong>
+
+              <from>
+                <div className="api-key-fld">
+                  <TextField id="outlined-search" label="API Key" variant="outlined" />
+                </div>
+                <div className="save-btn">
+                  <Button variant="contained" color="primary"> Save </Button>
+                </div>
+              </from>
+
+             </Typography>
+            </Container>
+          </React.Fragment>
       </div>
     )
   }
