cf35f1a78eea0dc4f07a2161f5f2f5417eec5797
Sourov <sourovfci@gmail.com>
2021-07-07 19:41:50 +0600

    Sanitize user input in shortcode

--- includes/Front/Shortcode.php
+++ includes/Front/Shortcode.php
@@ -8,63 +8,75 @@ class Shortcode {
 	 * Constructor
 	 */
 	public function __construct() {
-		add_shortcode('col-map', [$this, 'col_map_shortcode']);
+		add_shortcode( 'col-map', array( $this, 'col_map_shortcode' ) );
 	}
 
-	public function col_map_shortcode($attr) {
-		wp_enqueue_script( 'google_map_js');
+	/**
+	 * Display map in the page
+	 *
+	 * @param $attr
+	 *
+	 * @return string
+	 */
+	public function col_map_shortcode( $attr ) {
+		wp_enqueue_script( 'google_map_js' );
 
-		$defalut = array(
-			'id'	=>'',
-		);
+		$attrs = shortcode_atts( array(
+			'id' => '',
+		), $attr );
 
-		$map_id = shortcode_atts($defalut,$attr);
-		$post_data   = get_post($map_id['id']);
+		$map_id    = sanitize_text_field( $attrs['id'] );
+		$post_data = get_post( $map_id );
 
-		//post_content data
-		if(isset($post_data->post_content)){
-			$map_info = maybe_unserialize($post_data->post_content);
-			$marker = $map_info['markerIcon'];
-			$marker_url = COL_DIR_URL . 'assets/icons/'.$marker['color'].'_'.$marker['type'].'.png';
-			
-			$lat_lng = $map_info['mapPosition'];
+		if ( ! $post_data ) {
+			return '<p>There is no such a map! Please set the map, or check the shortcode.<p>';
 		}
-		else{
-			echo "There is no such a map! Please set map, or check the shortcode";
+
+		if ( 'col_maps' !== $post_data->post_type ) {
+			return '<p>You have not set a correct map ID, Please copy the shortcode from Location page</p>';
 		}
 
-		//return print_r($marker);
-		
-		ob_start(); ?>
-			<div id="col-map-<?php echo $post_data->ID; ?>" style="height: 400px; width: 700px; "></div>
+		$map_info = maybe_unserialize( $post_data->post_content );
 
-			<script type="text/javascript">
-				let map;
-				const myLatLng = { 
-					lat: <?php echo $lat_lng['lat']; ?>, 
-					lng: <?php echo $lat_lng['lng']; ?> 
-				};
-				var mapOptions = {
-					zoom: 4,
-					center: myLatLng
-				};
+		if ( ! $map_info ) {
+			return '<p>Your map is not set with proper data, Please check the shortcode in Location page.</p>';
+		}
 
-				function initMap() {
-					const image ="<?php echo $marker_url; ?>";
-				  	map = new google.maps.Map(document.getElementById("col-map-<?php echo $post_data->ID; ?>"),
-				  		mapOptions
-				  	);
+		$marker     = $map_info['markerIcon'];
+		$marker_url = COL_DIR_URL . 'assets/icons/' . $marker['color'] . '_' . $marker['type'] . '.png';
+		$lat_lng    = $map_info['mapPosition'];
 
-				  	new google.maps.Marker({
-				    	position: myLatLng,
-				    	map,
-				    	icon: image
-					});
-				}
+		ob_start();
+		?>
+		<div id="col-map-<?php echo $post_data->ID; ?>" style="height: 400px; width: 700px; "></div>
 
-			</script>
+		<script type="text/javascript">
+			var map;
+			var myLatLng = {
+				lat: <?php echo $lat_lng['lat']; ?>,
+				lng: <?php echo $lat_lng['lng']; ?>
+			};
+			var mapOptions = {
+				zoom: 4,
+				center: myLatLng
+			};
 
-		<?php $map = ob_get_clean();
-		return $map;
+			function initMap() {
+				const image = "<?php echo $marker_url; ?>";
+				map = new google.maps.Map( document.getElementById( "col-map-<?php echo $post_data->ID; ?>" ),
+					mapOptions
+				);
+
+				new google.maps.Marker( {
+					position: myLatLng,
+					map,
+					icon: image
+				} );
+			}
+		</script>
+
+		<?php
+			return ob_get_clean();
 	}
+
 }
